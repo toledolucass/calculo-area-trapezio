4bda056524d7b0bbf2f45a041307fbf2d6434299
toledolucass <toledo.lucasp@gmail.com>
2023-03-15 16:42:35 -0300

    código para calculo da área do trapézio

---
+++ ex014.py
@@ -0,0 +1,8 @@
+print("CÁLCULO ÁREA DO TRAPÉZIO")
+print("Qual o valor das bases? ")
+base1 = float(input())
+base2 = float(input())
+print("Qual a altura?" )
+altura = float(input())
+area = (base1 + base2)*altura/2
+print("A área do trapézio é: ", area)
